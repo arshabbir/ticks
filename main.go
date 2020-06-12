94220ef73994c05753ee4b19dfd95ec76dee347d
arshabbir <shabbir.hussain@netxcell.com>
2020-06-12 16:02:24 +0530

    Initial commit v1

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+var mut sync.Mutex
+var smsg string = "tick"
+var mmsg string = "tock"
+var hmsg string = "bong"
+var msg string
+
+var gque = make(chan string, 10)
+
+func main() {
+
+	wg.Add(5)
+
+	go print()
+	go tick()
+	go tock()
+	go bong()
+	go input()
+
+	wg.Wait()
+
+}
+
+func print() {
+	for {
+
+		fmt.Println(time.Now(), <-gque)
+		time.Sleep(time.Second)
+
+	}
+}
+
+func tick() {
+	for {
+
+		mut.Lock()
+		time.Sleep(time.Second)
+		gque <- smsg
+		mut.Unlock()
+
+	}
+	wg.Done() //no need for this
+}
+
+func tock() {
+	for {
+
+		time.Sleep(time.Minute)
+		mut.Lock()
+		gque <- mmsg
+		mut.Unlock()
+
+	}
+	wg.Done() //no need for this
+}
+
+func bong() {
+	hr := 0
+	for {
+		time.Sleep(time.Hour)
+		mut.Lock()
+		gque <- hmsg
+		mut.Unlock()
+
+		hr++
+		if hr > 2 {
+			//Exit after 3 hrs
+			os.Exit(0)
+		}
+
+	}
+	wg.Done() //no need for this
+
+}
+
+func input() {
+
+	for {
+		time.Sleep(time.Microsecond)
+		sc := bufio.NewScanner(os.Stdin)
+
+		sc.Scan()
+
+		msg = sc.Text()
+
+		cmd := strings.Split(msg, " ")
+
+		if cmd == nil || len(cmd) != 2 {
+			fmt.Println("Syntax error :  s/m/h newstring")
+		}
+
+		if cmd[0] == "s" {
+			smsg = cmd[1]
+		}
+		if cmd[0] == "m" {
+			mmsg = cmd[1]
+
+		}
+		if cmd[0] == "h" {
+			hmsg = cmd[1]
+
+		}
+
+	}
+}
---
+++ main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestTick(t *testing.T) {
+
+}
+
+func TestTock(t *testing.T) {
+
+}
+
+func TestBong(t *testing.T) {
+
+}
+
+func TestInput(t *testing.T) {
+
+}
+
+func TestPrint(t *testing.T) {
+
+}
